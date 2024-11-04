6b8a74a9e4176fcf73da55ee7265cf38e51a2d4f
itsanishsingh <itzanishsingh@gmail.com>
2024-11-04 15:01:21 +0530

    Added divide functionality

--- Main.py
+++ Main.py
@@ -5,14 +5,19 @@ def add(a, b):
 def multiply(a, b):
     return a * b
 
+
 def sub(a, b):
     return a - b
+
+
+def divide(a, b):
+    return a / b
+
 
 var1 = 3
 var2 = 7
 
-print(sub(3,7))
+print(sub(3, 7))
 print(add(3, 7))
 print(multiply(var1, var2))
-
-
+print(divide(var2, var1))
